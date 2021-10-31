c1f347a6ea41d462199cb078c68e5a8425945e72
Nu Huyen Trang Pham <nu-huyen-trang.pham@etu.utc.fr>
2021-10-31 12:54:23 +0100

    delete recent added sport place

--- test/function_test.ts
+++ test/function_test.ts
@@ -63,30 +63,39 @@ describe("TEST ALL QUERIES", function () {
         it('Get sport places with id', async done => {
             log.info("======================= Get sport places with id =========================");
             var params = {
-                type_sport: ["basketball", "football"]
+                type_sport_place: "sport_complex",
+                id: "2"
             };
-            var return_value = await sport_place.getSportPlacesWithTypeSport(params.type_sport);
+            var return_value = await sport_place.getSportPlaceWithID(params.type_sport_place, params.id);
             log.info("Response returned: " + return_value);
             let tool = [
-                {  
-                    "id":1,
-                    "adress":"2 Résidence de la Bergerie, 91300 Massy",
-                    "type":"sport_complex",
-                    "sports":[{"name":"Football","nbPlayers":50}]
-                },
                 {
-                    "id":2,
                     "adress":"7 Bd Gaspard Monge, 91120 Palaiseau",
                     "type":"sport_complex",
-                    "sports":[{"name":"Basketball","nbPlayers":20}]
-                },
-                {
-                    "id":3,
-                    "adress":"Rte de Saclay, 91120 Palaiseau",
-                    "type":"field",
-                    "sports":[{"name":"Basketball","nbPlayers":60}]
+                    "openHours":["Mercredi: from 10h to 17h","Mardi: from 11h to 20h"],
+                    "sports":["Basketball: 20","Volleyball: 30"]
                 }
             ];
+            expect(return_value == tool);
+            done();
+        });
+
+        it('Add new sport place', async done => {
+            log.info("======================= Add new sport place =========================");
+            var params = {
+                adress: "26 Grand Rue, 13115 Saint-Paul-lez-Durance",
+                type: "field",
+                country: "FR",
+                day: ["Lundi","Samedi"],
+                from: ["9h","10h"],
+                to: ["18h","17h"],
+                type_sport: ["Natation"]
+            };
+            var return_value = await sport_place.addNewSportPlace(params.adress, params.type, params.country, 
+                params.day, params.from, params.to, params.type_sport);
+            log.info("Response returned: " + return_value);
+            let tool = "Adding new sport place sucessfully";
+            
             expect(return_value == tool);
             done();
         });
